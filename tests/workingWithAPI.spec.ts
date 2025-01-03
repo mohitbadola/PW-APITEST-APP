7d847eab74d6af1c051d264f1d147ba767e3df86
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-03 15:38:25 +0530

    Create tags.json and mock api

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,9 +1,16 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
+import tags from "../test-data/tags.json";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.beforeEach(async ({ page }) => {
+  await page.route("*/**/api/tags", async (route) => {
+    await route.fulfill({
+      body: JSON.stringify(tags),
+    });
+  });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await page.goto("http://conduit.bondaracademy.com/");
 });
-
+test("has title", async ({ page }) => {
+  // await expect(page.locator('.navbar-brand')).toHaveText('conduit')
+  await expect(page.locator(".sidebar")).toContainText("automation");
+});
